7e985679d522ad7179291dfaf85a0b7955ba752e
codekot <gennadii.shamshurin@gmail.com>
2023-02-10 17:45:41 +0400

    Add simple Choropleth representation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import geopandas as gpd
 import folium
 
 
@@ -6,8 +7,23 @@ center_latitude = 57
 center_longitude = 52
 zoom_level = 8
 
-
 base_map = folium.Map(location=[center_latitude, center_longitude], zoom_start=zoom_level)
+
+geojson_file = "udmurtia_hex_without_towns.geojson"
+df = gpd.read_file(geojson_file)
+
+
+folium.Choropleth(
+    geo_data=geojson_file,
+    name="choropleth",
+    data=df,
+    columns=["fid", "NUMPOINTS"],
+    key_on="properties.fid",
+    fill_color="YlGn",
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name="Population",
+).add_to(base_map)
 
 app = Flask(__name__)
 
