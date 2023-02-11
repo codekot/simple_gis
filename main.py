4c1fe80228655cac4998a9b51378f5fe6cec8069
codekot <boobookish@gmail.com>
2023-02-11 12:28:57 +0400

    Trying natural breaks method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import geopandas as gpd
 import folium
+import jenkspy
 
 
 center_latitude = 57
@@ -11,6 +12,13 @@ base_map = folium.Map(location=[center_latitude, center_longitude], zoom_start=z
 
 geojson_file = "udmurtia_hex_without_towns.geojson"
 df = gpd.read_file(geojson_file)
+
+pop_data = df["NUMPOINTS"].tolist()
+num_classes = 15
+breaks = jenkspy.jenks_breaks(pop_data, n_classes=num_classes)
+print(breaks)
+test_breaks = [ 347.0, 489.0, ]
+bin_ranges = [min(pop_data)] + test_breaks + [max(pop_data)]
 
 
 folium.Choropleth(
@@ -23,6 +31,7 @@ folium.Choropleth(
     fill_opacity=0.7,
     line_opacity=0.2,
     legend_name="Population",
+    bin=bin_ranges,
 ).add_to(base_map)
 
 app = Flask(__name__)
