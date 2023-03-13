90548f4fe4b3a04755e54357a5d6fd808f05bd35
codekot <gennadii.shamshurin@gmail.com>
2023-03-13 16:39:45 +0400

    add color to choropleth

--- static/map.js
+++ static/map.js
@@ -12,11 +12,34 @@ var lineStyle = {
     "fill": false
 };
 
+function getColor(value) {
+  if (value > 5000) {
+    return "#800026";
+  } else if (value > 1000) {
+    return "#BD0026";
+  } else if (value > 500) {
+    return "#E31A1C";
+  } else {
+    return "gray";
+  }
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.NUMPOINTS),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
 fetch("/data")
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, {
-            style: lineStyle
+            style: style
         }).addTo(map);
         console.log(data);
   })
